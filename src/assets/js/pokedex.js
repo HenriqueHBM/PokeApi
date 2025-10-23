3e30f5772bcd3a6ce7c1c6324022a771c3017a42
HenriqueHBM <henriquemadeira1@gmail.com>
2025-10-23 07:54:02 -0300

    build:add tipo modulo, para poder usar as funcao em outros script; refactor: separacao dos arquivos e add comentarios

---
+++ src/assets/js/pokedex.js
@@ -0,0 +1,130 @@
+//Importanto as funcoes do outro arquivo js
+import { tiposPoke } from "./tipos.js"
+import { listarPokemonRegiaoTipo } from "./tipos.js";
+
+const PAGE_SIZE = 4;
+async function carregarTemplateTipos() {
+    const tipos = await tiposPoke();
+    const lista = document.getElementById("tipo-list");
+
+    for (const tipo_poke of tipos) {
+        //declarando uma variavel que esta recebendo a regiao pela url do navegador
+        const params = new URLSearchParams(document.location.search);
+        const regiao = params.get("regiao");
+
+        //declarando variaveis que estao recebendo uma div
+        const div_container_tipo = document.createElement('div');
+        div_container_tipo.classList.add('container_tipo');
+
+        let page = 0;
+        //lista dos pokemons filtrada pela regiao, e o tipo
+        const lista_pokemons = await listarPokemonRegiaoTipo(tipo_poke.name, regiao);
+        const total_pagina = Math.max(1, Math.ceil(lista_pokemons.length / PAGE_SIZE));
+
+        //construcao do container das separacoes por tipo de pokemon
+        div_container_tipo.innerHTML = `
+            <div class='header_tipo header_cor_tipo_${tipo_poke.name}'> 
+                <button class='btn_prev btn_card' id='btn_prev_${tipo_poke.name}'> 
+                    <img src='/public/icons/prev_icon.png' class='icon_btn_card' />
+                </button>
+                <div class='div_text_type'>
+                    <div class='icon_tipo'>    
+                        <img src='/public/icons/icons_tipo/${tipo_poke.name}.svg' width='35rem' height='35rem' class='img_icon_tipo' />
+                    </div>
+                    <div class='text_tipo'>${tipo_poke.name} </div>
+                </div>
+                <button  class='btn_next btn_card' id='btn_next_${tipo_poke.name}'> 
+                    <img src='/public/icons/next_icon.png' class='icon_btn_card' />
+                </button>
+            </div>
+            <div class='body_tipo body_cor_tipo_${tipo_poke.name}' id='body_tipo_${tipo_poke.name}'> 
+
+            </div>
+            <div class='footer-tipo header_cor_tipo_${tipo_poke.name}'> 
+                <span id='prev_pagination_${tipo_poke.name}'> ${page + 1 } </span>
+                <span>de</span>
+                <span id='next_pagination_${tipo_poke.name}'> ${total_pagina} </span>
+            </div>
+        `;
+        
+        function carregarCards(){
+            const ini = page * PAGE_SIZE; // 0
+            const fim = ini + PAGE_SIZE;  // 4
+            const limit_pokemon_carrossel = lista_pokemons.slice(ini, fim); // lista dos pokemons sendo cortada, ini e fim das qtde a mostrar
+
+            const card_pokemon = document.getElementById(`body_tipo_${tipo_poke.name}`);
+            //limpando caso ja tenha cards carregados
+            card_pokemon.innerHTML = "";
+
+            if(limit_pokemon_carrossel.length == 0){
+                const div_card_vazio = document.createElement('div');
+                div_card_vazio.classList.add("card_tipo_vazio");
+                div_card_vazio.innerText = "SEM POKÉMON'S DESSA REGIÃO E TIPO"
+                card_pokemon.append(div_card_vazio);
+            }else{
+                limit_pokemon_carrossel.forEach(poke => {
+                    const card_a = document.createElement("a");
+                    card_a.classList.add("card_pokemon", `card_cor_tipo_${tipo_poke.name}`);
+                    card_a.href = `/src/views/pokemon.html?id=${poke.id}`;
+                    card_a.innerHTML = `
+                        <div class='link-card-img'>
+                            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png' width='100%' height='260rem' style='border-radius: 20px' />
+                        </div>
+                        <div class='link-card'>
+                            <span class='text-link-card'><b>#${poke.id}</b></span> <br>
+                            <span class='text-link-card'>${poke.name}</span>
+                        </div>
+                    `;
+                    card_pokemon.appendChild(card_a);
+                });
+            }
+            btn_prev.disabled = page == 0; // caso contador esteja com 0 disabilita o botao
+            btn_next.disabled = page >= total_pagina - 1; // caso o total por pagina tenha menos que o contador, desabilita o botao
+        }
+
+        //append dessas variaveis
+        lista.append(div_container_tipo);
+
+        //apos carregar os cards na tela, inicializa as variaveis onde tiver tal id
+        const btn_prev = document.getElementById(`btn_prev_${tipo_poke.name}`);
+        const btn_next = document.getElementById(`btn_next_${tipo_poke.name}`);
+        carregarCards(); // inicializando a primeira vez
+
+        btn_prev.addEventListener("click", () => { 
+            // caso o contador(no caso), tenha mais que 0, deixa subtrair
+            if(page > 0){
+                page--;
+                //refaz os cards dentro do container
+                carregarCards();
+                document.getElementById(`prev_pagination_${tipo_poke.name}`).innerText = page + 1;
+            }
+        });
+
+        btn_next.addEventListener("click", () => {
+            if(page <= total_pagina){ // caso o contador seja meno que a quantidade, deixa acrescentar 
+                page++;
+                carregarCards();
+                document.getElementById(`prev_pagination_${tipo_poke.name}`).innerText = page + 1;
+            }
+        });
+    };
+}
+
+
+//funcao que troca os pokemons com base no que tem na url e seta o select com o mesmo
+function atualiza_select_regiao(){
+    const select = document.getElementById('input_select_region');
+    const params = new URLSearchParams(document.location.search);
+    //setanto o select com a regiao da url
+    select.value = params.get('regiao');
+    //event para quando trocado a opcao no select
+    select.addEventListener('change', function(e){
+        //setando na url a nova regiao
+        params.set('regiao', select.value);
+        //atualiza de fato a url com os parametros
+        window.location.search = params;
+    })
+}
+
+atualiza_select_regiao();
+carregarTemplateTipos();
--- src/assets/js/tipos.js
+++ src/assets/js/tipos.js
@@ -1,4 +1,5 @@
-async function tiposPoke() {
+//export para poder usar em outros scripts
+export async function tiposPoke() {
     // pegar a regiao
     const resposta = await fetch("https://pokeapi.co/api/v2/type/");
     const dados = await resposta.json();
@@ -9,17 +10,19 @@ async function tiposPoke() {
     return tiposValidos;
 }
 
-async function listagemRegioes(regiao) {
+async function retornaUrlPokedexRegiao(regiao) {
     const resp = await fetch(`https://pokeapi.co/api/v2/region/${regiao}`);
     const dados = await resp.json();
-    return dados.pokedexes[0].url; // pega a primeira pokédex da região
+    return dados.pokedexes[0].url; // pega a primeira pokédex da região, ou primeira url da pokedex da regiao
 }
 
 async function listarPokemonsDaRegiao(url) {
     const resp = await fetch(url);
     const dados = await resp.json();
 
+    //tratamento das informacoes para retornar apenas o id e nome do pokemon
     return dados.pokemon_entries.map(dado => {
+        //cortando a url de dentro da pokedex do pokemon para pegar apenas o id do mesmo
         const id = dado.pokemon_species.url.split('pokemon-species/')[1].slice(0, -1);
         return { id, name: dado.pokemon_species.name };
     });
@@ -30,145 +33,21 @@ async function listarPokemonsDoTipo(tipo) {
     const dados = await resp.json();
 
     return dados.pokemon.map(p => {
+        //cortando a url do pokemon para pegar apenas o id do mesmo
         const id = p.pokemon.url.split('pokemon/')[1].slice(0, -1);
         return { id, name: p.pokemon.name };
     });
 }
 
-async function listarPokemonRegiaoTipo(tipo, regiao) {
-    // Busca lista de pokémons da região
-    const urlPokedex = await listagemRegioes(regiao);
+export async function listarPokemonRegiaoTipo(tipo, regiao) {
+    // Retorna a url da pokedex da regiao
+    const urlPokedex = await retornaUrlPokedexRegiao(regiao);
+    //com base na pokedex, retorna o id e nome do pokemon com base na regiao
     const pokemonsRegiao = await listarPokemonsDaRegiao(urlPokedex);
-
-    //  Busca lista de pokémons do tipo
+    //  Busca lista de pokémons pelo tipo dele
     const pokemonsTipo = await listarPokemonsDoTipo(tipo);
-
     //  Faz interseção das listas (região ∩ tipo)
     const idsTipo = new Set(pokemonsTipo.map(p => p.id));
-
+    //filtra os pokemons da regiao pelo tipo de cada, tipo: se na regiao tem o id do pokemon de certo tipo, pega esse pokemon
     return pokemonsRegiao.filter(p => idsTipo.has(p.id));
 }
-
-const PAGE_SIZE = 4;
-async function carregarTemplateTipos() {
-    const tipos = await tiposPoke();
-    const lista = document.getElementById("tipo-list");
-
-    for (const tipo_poke of tipos) {
-        //declarando uma variavel que esta recebendo a regiao pela url do navegador
-        const params = new URLSearchParams(document.location.search);
-        const regiao = params.get("regiao");
-
-        //declarando variaveis que estao recebendo uma div
-        const div_container_tipo = document.createElement('div');
-        div_container_tipo.classList.add('container_tipo');
-
-        let page = 0;
-        const lista_pokemons = await listarPokemonRegiaoTipo(tipo_poke.name, regiao);
-        const total_pagina = Math.max(1, Math.ceil(lista_pokemons.length / PAGE_SIZE));
-
-        //construcao do container das separacoes por tipo de pokemon
-        div_container_tipo.innerHTML = `
-            <div class='header_tipo header_cor_tipo_${tipo_poke.name}'> 
-                <button class='btn_prev btn_card' id='btn_prev_${tipo_poke.name}'> 
-                    <img src='/public/icons/prev_icon.png' class='icon_btn_card' />
-                </button>
-                <div class='div_text_type'>
-                    <div class='icon_tipo'>    
-                        <img src='/public/icons/icons_tipo/${tipo_poke.name}.svg' width='35rem' height='35rem' class='img_icon_tipo' />
-                    </div>
-                    <div class='text_tipo'>${tipo_poke.name} </div>
-                </div>
-                <button  class='btn_next btn_card' id='btn_next_${tipo_poke.name}'> 
-                    <img src='/public/icons/next_icon.png' class='icon_btn_card' />
-                </button>
-            </div>
-            <div class='body_tipo body_cor_tipo_${tipo_poke.name}' id='body_tipo_${tipo_poke.name}'> 
-
-            </div>
-            <div class='footer-tipo header_cor_tipo_${tipo_poke.name}'> 
-                <span id='prev_pagination_${tipo_poke.name}'> ${page + 1 } </span>
-                <span>de</span>
-                <span id='next_pagination_${tipo_poke.name}'> ${total_pagina} </span>
-            </div>
-        `;
-        
-        function carregarCards(){
-            const ini = page * PAGE_SIZE; // 0
-            const fim = ini + PAGE_SIZE;  // 4
-            const limit_pokemon_carrossel = lista_pokemons.slice(ini, fim); // lista dos pokemons sendo cortada, ini e fim das qtde a mostrar
-
-            const card_pokemon = document.getElementById(`body_tipo_${tipo_poke.name}`);
-            //limpando caso ja tenha cards carregados
-            card_pokemon.innerHTML = "";
-
-            if(limit_pokemon_carrossel.length == 0){
-                const div_card_vazio = document.createElement('div');
-                div_card_vazio.classList.add("card_tipo_vazio");
-                div_card_vazio.innerText = "SEM POKÉMON'S DESSA REGIÃO E TIPO"
-                card_pokemon.append(div_card_vazio);
-            }else{
-                limit_pokemon_carrossel.forEach(poke => {
-                    const card_a = document.createElement("a");
-                    card_a.classList.add("card_pokemon", `card_cor_tipo_${tipo_poke.name}`);
-                    card_a.href = `/src/views/pokemon.html?id=${poke.id}`;
-                    card_a.innerHTML = `
-                        <div class='link-card-img'>
-                            <img src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${poke.id}.png' width='100%' height='260rem' style='border-radius: 20px' />
-                        </div>
-                        <div class='link-card'>
-                            <span class='text-link-card'><b>#${poke.id}</b></span> <br>
-                            <span class='text-link-card'>${poke.name}</span>
-                        </div>
-                    `;
-                    card_pokemon.appendChild(card_a);
-                });
-            }
-            btn_prev.disabled = page == 0; // caso contador esteja com 0 disabilita o botao
-            btn_next.disabled = page >= total_pagina - 1; // caso o total por pagina tenha menos que o contador, desabilita o botao
-        }
-
-        //append dessas variaveis
-        lista.append(div_container_tipo);
-
-        const btn_prev = document.getElementById(`btn_prev_${tipo_poke.name}`);
-        const btn_next = document.getElementById(`btn_next_${tipo_poke.name}`);
-        carregarCards(); // inicializando a primeira vez
-
-        btn_prev.addEventListener("click", () => { 
-            // caso o contador(no caso), tenha mais que 0, deixa subtrair
-            if(page > 0){
-                page--;
-                carregarCards();
-                document.getElementById(`prev_pagination_${tipo_poke.name}`).innerText = page + 1;
-            }
-        });
-
-        btn_next.addEventListener("click", () => {
-            if(page <= total_pagina){ // caso o contador seja meno que a quantidade, deixa acrescentar 
-                page++;
-                carregarCards();
-                document.getElementById(`prev_pagination_${tipo_poke.name}`).innerText = page + 1;
-            }
-        });
-    };
-}
-
-
-//funcao que troca os pokemons com base no que tem na url e seta o select com o mesmo
-function atualiza_select_regiao(){
-    const select = document.getElementById('input_select_region');
-    const params = new URLSearchParams(document.location.search);
-    //setanto o select com a regiao da url
-    select.value = params.get('regiao');
-    //event para quando trocado a opcao no select
-    select.addEventListener('change', function(e){
-        //setando na url a nova regiao
-        params.set('regiao', select.value);
-        //atualiza de fato a url com os parametros
-        window.location.search = params;
-    })
-}
-
-atualiza_select_regiao();
-carregarTemplateTipos();
